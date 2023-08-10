27b1f40aced9bc303b50bf645c10aaea87920608
AubBantolo <jhuliet14@gmail.com>
2023-08-10 08:41:46 +0800

    -A

--- src/main/java/com/tw/StringCalculator.java
+++ src/main/java/com/tw/StringCalculator.java
@@ -1,7 +1,25 @@
 package com.tw;
 
+import java.util.Arrays;
+
 public class StringCalculator {
+
     public int add(String string) {
-        return 0;
+        if (string.isEmpty()) {
+            return 0;
+        }
+
+        String separator = ",";
+
+        if (string.startsWith("//")) {
+            separator = string.substring(2, 3);
+            string = string.substring(4);
+        }
+
+        String[] numbers = string.split("[" + separator + "\n]");
+
+        return Arrays.stream(numbers)
+                .mapToInt(s -> Integer.parseInt(s))
+                .sum();
     }
 }
